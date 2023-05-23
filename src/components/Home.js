93f1d8791a81a1cc18963b220ca3370c6a349671
Sahana <sahana@sainikpod.org>
2023-05-23 16:57:18 +0530

    BG fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import bg from '../assets/bg.jpg'
+import bgSmall from '../assets/bgSmall.jpeg'
 
 export default function Home() {
+  const [screenSize, getDimension] = useState({
+    dynamicWidth: window.innerWidth,
+    dynamicHeight: window.innerHeight,
+  })
+
+  const [bgImage, setBgImage] = useState(bg)
+
+  const setDimension = () => {
+    getDimension({
+      dynamicWidth: window.innerWidth,
+      dynamicHeight: window.innerHeight,
+    })
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', setDimension)
+    if (screenSize.dynamicWidth <= 770) {
+      setBgImage(bgSmall)
+    } else {
+      setBgImage(bg)
+    }
+    return () => {
+      window.removeEventListener('resize', setDimension)
+    }
+  }, [screenSize])
+
   return (
     <section id="home">
-     
-
       <div
-        class="bg-fixed ..."
+        class="bg-fixed bg-[center]"
         style={{
-          backgroundImage: `url(${bg})`,
+          backgroundImage: `url(${bgImage})`,
           borderBottomLeftRadius: '35rem',
           borderBottomRightRadius: '35rem',
+
+        //   '@media screen and (min-width: 0px) and (max-width: 400px)': {
+        //     backgroundImage: `url(${bgS})` /* show it on small screens */,
+        //   },
         }}
       >
         <section
@@ -21,7 +50,7 @@ export default function Home() {
             borderBottomRightRadius: '35rem',
           }}
         >
-          <h1 className="text-6xl flex-initial text-white font-bold text-center  leading-none lg:leading-snug home-name">
+          <h1 className="text-5xl lg:text-6xl flex-initial text-white font-bold text-center  leading-none lg:leading-snug home-name">
             Electrify Your Business with SainikPod
           </h1>
         </section>
