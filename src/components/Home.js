961146f04fefe5cc5b03d8a91700160fd37322a3
sahr2397 <bokehlenz@gmail.com>
2024-01-07 12:58:30 +0530

    typo fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -58,14 +58,3 @@ export default function Home() {
     </section>
   )
 }
-
-// <main>
-//       <img
-//         src="https://cdn.vox-cdn.com/thumbor/znw2FIDAIg1yK4SzJPHORaWfyoI=/1400x788/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/11477517/shutterstock_678147802.jpg"
-//         alt="monstera leaf"
-//         className="absolute cover w-full h-full"
-//       />
-//       <section className="relative flex justify-center min-h-screen pt-12 lg: pt-64 px-8">
-//         <h1 className="text-6xl text-white cursive font-bold leading-none lg:leading-snug home-name">Welcome to my Homepage</h1>
-//       </section>
-//     </main>
